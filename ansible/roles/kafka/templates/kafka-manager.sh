58a04d1be89cdd8efa12b919ca2751fae186402f
Giacomo Marciani <giacomo.marciani@gmail.com>
2017-03-22 14:03:40 +0100

    kafka role

---
+++ ansible/roles/kafka/templates/kafka-manager.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+case "$1" in
+start)  echo "[kafka-manager] starting kafka"
+        {{ kafka_home }}/bin/zookeeper-server-start.sh {{ kafka_home }}/config/zookeeper.properties
+        {{ kafka_home }}/bin/kafka-server-start.sh -daemon {{ kafka_home }}/config/server.properties
+        {{ kafka_home }}/bin/kafka-topics.sh --create --topic {{ kafka_topic }} --zookeeper localhost:{{ kafka_zookeeper_port }} --partitions 1 --replication-factor 1
+        echo "[kafka-manager] waiting kafka to warm up (max: {{ kafka_start_wait }} seconds)"
+        end="$(( SECONDS + {{ kafka_start_wait }} ))"
+        while true; do
+            [[ "{{ kafka_topic }}" = "$({{ kafka_home }}/bin/kafka-topics.sh --list --zookeeper localhost:{{ kafka_http_port }})" ]] && break
+            [[ "${SECONDS}" -ge "${end}" ]] && exit 1
+            sleep "{{ kafka_start_pool }}"
+        done
+        echo "[kafka-manager] kafka started"
+        ;;
+stop)   echo "[kafka-manager] stopping kafka..."
+        {{ kafka_home }}/bin/zookeeper-server-stop.sh
+        echo "[kafka-manager] kafka stopped"
+        ;;
+restart) echo "[kafka-manager] restarting kafka..."
+        service kafka stop
+        service kafka start
+        echo "[kafka-manager] kafka restarted"
+        ;;
+reload|force-reload) echo "[kafka-manager] Not yet implemented"
+        ;;
+*)      echo "Usage: kafka.sh {start|stop|restart|reload|force-reload}"
+        exit 2
+        ;;
+esac
+exit 0
