1ce4490f3bb01234e16d4cdcd78f7b8b00efb3fb
Giacomo Marciani <giacomo.marciani@gmail.com>
2017-04-14 10:32:38 +0200

    sync

--- .credentials.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export AWS_ACCESS_KEY_ID='AKIAIPR3DKOM2V3SBVUQ'
-export AWS_SECRET_ACCESS_KEY='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'
-export AWS_KEYPAIR_NAME='gmarciani'
-export AWS_KEYPAIR_PATH='/home/gmarciani/.aws/gmarciani.pem'
